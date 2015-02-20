9edf783bd3bb14f5b527653c78142c55dbee5b97
sathorael <sarielirc@gmail.com>
2015-02-20 15:21:33 +0200

    alussprite puuttuu

--- Luolalentely/PlayerInputComponent.cpp
+++ Luolalentely/PlayerInputComponent.cpp
@@ -15,23 +15,24 @@ PlayerInputComponent::~PlayerInputComponent()
 
 void PlayerInputComponent::update(sf::Time &elapsed)
 {
+	const float speed = 20.f;
 	b2Vec2 velocity(0, 0);
 
 	if (sf::Keyboard::isKeyPressed(sf::Keyboard::A))
 	{
-
+		velocity.x = -speed;
 	}
 	else if (sf::Keyboard::isKeyPressed(sf::Keyboard::D))
 	{
-
+		velocity.x = speed;
 	}
 	if (sf::Keyboard::isKeyPressed(sf::Keyboard::W))
 	{
-
+		velocity.y = -speed;
 	}
 	else if (sf::Keyboard::isKeyPressed(sf::Keyboard::S))
 	{
-
+		velocity.y = speed;
 	}
 
 	getOwner()->getComponent<ColliderComponent>()->getBody()->SetLinearVelocity(velocity);
